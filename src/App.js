fbd09dc44e6180adc6b536bf576bb2159ebc7f97
lipsilverhand <williamhuynh99@gmail.com>
2024-02-08 18:22:51 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    const [tasks, setTasks] = useState([
+        {id:1, name: "Record Lectures", complete: true}, 
+        {id:2, name: "Watch course", complete: false}, 
+        {id:3, name: "Edit code", complete: true},
+        {id:4, name: "Take notes", complete: true}
+    ]);
+
+    const [show, setShow] = useState(true)
+
+    function handleDelete(id) {
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+
+
+
+    return (
+        <div className='App'>
+            <h1>Task List</h1>
+            <ul>
+                <button className='toggle' onClick={() => setShow(!show)}>Toggle</button>
+                { show && tasks.map((task) => (
+                    <li key={task.id} className={task.complete ? "completed" : "incomplete"}>
+                        <span>{task.id} - {task.name}</span>
+                        <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App;
+
+
+
